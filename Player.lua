95acf45773840eeea69b527f56f48e64fa7a733f
snpefk <snpefk@gmail.com>
2017-12-02 22:08:12 +0700

    menu: enable "Start button"

--- Player.lua
+++ Player.lua
@@ -2,17 +2,17 @@ Player = {}
 
 Player.x = 0
 Player.y = 0
-Player.width = 18*2
-Player.height = 18*2
+Player.width = 18 * 2
+Player.height = 18 * 2
 Player.way = 0
 
 local anim8 = require("lib/anim8")
 
 local scale = 2
 local speed = 2
-local standAnim = nil
+local standAnim
 
-local image = nil
+local image
 local animations = {}
 --[[
 	Stand = 0
@@ -23,74 +23,63 @@ local state = 0
 
 -- function Player.load(pathToStand)
 function Player.load(x, y, l)
-	Player.x = x
-	Player.y = y
-	Player.way = l
+    Player.x = x
+    Player.y = y
+    Player.way = l
 
-	image = love.graphics.newImage('dat/gph/fox.png') 
+    image = love.graphics.newImage('dat/gph/fox.png')
 
-	Player.addAnim('stand', 18, 18, 0, 0, 4, 0.1)
-	Player.addAnim('run', 18, 18, 0, 18, 4, 0.1)
+    Player.addAnim('stand', 18, 18, 0, 0, 4, 0.1)
+    Player.addAnim('run', 18, 18, 0, 18, 4, 0.1)
 
-	-- img = love.graphics.newImage(pathToStand)
-	-- local g = anim8.newGrid(18, 18, img:getWidth(), img:getHeight())
-	-- standAnim = anim8.newAnimation(g('1-4',1), 0.2)
+    -- img = love.graphics.newImage(pathToStand)
+    -- local g = anim8.newGrid(18, 18, img:getWidth(), img:getHeight())
+    -- standAnim = anim8.newAnimation(g('1-4',1), 0.2)
 end
 
 function Player.addAnim(name, width, height, left, top, n, time)
-	local g = anim8.newGrid(width, height, image:getWidth(), image:getHeight(), left, top)
-	local str = '1-'..tostring(n)
-	animations[name] = anim8.newAnimation(g(str,1), time)
+    local g = anim8.newGrid(width, height, image:getWidth(), image:getHeight(), left, top)
+    local str = '1-' .. tostring(n)
+    animations[name] = anim8.newAnimation(g(str, 1), time)
 end
 
 function Player.animationUpdate(dt)
-	if state == 0 then
-		animations['stand']:update(dt)
-	elseif state == 1 then
-		animations['run']:update(dt)
-	elseif state == -1 then
-		animations['run']:update(dt)
-	end
+    if state == 0 then
+        animations['stand']:update(dt)
+    elseif state == 1 then
+        animations['run']:update(dt)
+    elseif state == -1 then
+        animations['run']:update(dt)
+    end
 end
 
 function Player.update(dt)
-	if love.keyboard.isDown('right') then
-		Player.way = Player.way + speed
-		state = 1
-	elseif love.keyboard.isDown('left') then
-		Player.way = Player.way - speed
-		state = -1
-	else
-		state = 0
-	end
+    if love.keyboard.isDown('right') then
+        Player.way = Player.way + speed
+        state = 1
+    elseif love.keyboard.isDown('left') then
+        Player.way = Player.way - speed
+        state = -1
+    else
+        state = 0
+    end
 
-
-
-	Player.animationUpdate(dt)
+    Player.animationUpdate(dt)
 end
 
 function Player.draw()
-	local anim = nil
-	if state == 0 then
-		anim = animations['stand']
-	elseif state == 1 then
-		anim = animations['run']
-		-- love.graphics.rotate(3.14/2)
-	elseif state == -1 then
-		anim = animations['run']
-	end
+    local anim
 
-	anim:draw(image, Player.x, Player.y, 0, 2, 2)
+    if state == 0 then
+        anim = animations['stand']
+    elseif state == 1 then
+        anim = animations['run']
+        -- love.graphics.rotate(3.14/2)
+    elseif state == -1 then
+        anim = animations['run']
+    end
 
+    anim:draw(image, Player.x, Player.y, 0, 2, 2)
 end
-
-
-
-
-
-
-
-
-
 
 return Player
--- menu.lua
+++ menu.lua
@@ -1,7 +1,11 @@
-STATE_SOUND = true
+local STATE_SOUND = true
+local startGameCallback
+
 
 function drawOptionsMenu()
     local cbSound = gui:checkbox(nul, { x = 255, y = 32, r = 8 })
+    local btnBack = gui:button('Back', { x = 256, y = 64, w = 128, h = gui.style.unit })
+
     cbSound.value = STATE_SOUND
     cbSound.style.labelfg = cbSound.style.fg
     cbSound.click = function(this)
@@ -21,8 +25,9 @@ function drawOptionsMenu()
         this.parent:click()
     end
 
-    local btnBack = gui:button('Back', { x = 256, y = 64, w = 128, h = gui.style.unit })
     btnBack.click = function()
+        gui:rem(cbSound)
+        gui:rem(cblSound)
         gui:rem(btnBack)
         drawMainMenu()
     end
@@ -32,6 +37,24 @@ function drawMainMenu()
     local btnStart = gui:button('Start', { x = 256, y = 32, w = 128, h = gui.style.unit })
     local btnOptions = gui:button('Options', { x = 256, y = 64, w = 128, h = gui.style.unit })
     local btnExit = gui:button('Exit', { x = 256, y = 96, w = 128, h = gui.style.unit })
+
+    btnStart.click = function()
+        gui:rem(btnStart)
+        gui:rem(btnOptions)
+        gui:rem(btnExit)
+
+        stage.load('stg/st1/map_b.png', 'stg/st1/map_f.png', 'stg/st1/description')
+        stage.newTexture('dat/img/block.png', 'block')
+        stage.newTexture('dat/img/empti.png', 'empti')
+
+        love.update = function(dt)
+            stage.update(dt)
+        end
+
+        love.draw = function()
+            stage.draw(0, 150)
+        end
+    end
 
     btnOptions.click = function()
         gui:rem(btnStart)
