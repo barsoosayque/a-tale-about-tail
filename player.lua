e80d182b38646fa7f2f19c009aaa374595cc1c23
Alexandr <leidenn2509@gmail.com>
2017-12-04 01:04:31 +0700

    a fall

--- player.lua
+++ player.lua
@@ -32,16 +32,16 @@ function Player.load(x, y, length)
     width = length
 
     img = love.graphics.newImage('dat/gph/fox.png')
-    Player.addAnim('standL', 18, 18,   0,  0, 	 4, 0.1)
+    Player.addAnim('standL', 18, 18,   0,  0,    4, 0.1)
     Player.addAnim('standR', 18, 18,   0,  18,   4, 0.1)
-    Player.addAnim('runL', 	 18, 18,   0,  36,   4, 0.1)
-    Player.addAnim('runR', 	 18, 18,   0,  54,   4, 0.1)
+    Player.addAnim('runL',   18, 18,   0,  36,   4, 0.1)
+    Player.addAnim('runR',   18, 18,   0,  54,   4, 0.1)
     Player.addAnim('jumpL',  18, 18,   0,  72,   2,   1)
     Player.addAnim('jumpR',  18, 18,   36, 72,   2,   1)
 end
 
 function Player.update(dt)
-	-- print('fly:'..tostring(fly)..' jump:'..tostring(jump)..'\ntime:'..tostring(t))
+    -- print('fly:'..tostring(fly)..' jump:'..tostring(jump)..'\ntime:'..tostring(t))
 
     if love.keyboard.isDown('left') then
         Player.speedX = -speed
@@ -69,7 +69,7 @@ function Player.draw(x, y)
     if run == 0 and side == 1 then
         anim = animations['standR']
     elseif run == 0 and side == -1 then
-        anim = animations['standL']    	
+        anim = animations['standL']     
     elseif run == 1 and side == 1 then
         anim = animations['runR']
     elseif run == 1 and side == -1 then
@@ -81,12 +81,12 @@ function Player.draw(x, y)
 
 
 
-    if fly == true and jump == true then
-		if side == -1 then
-			anim = animations['jumpL']
-		else
-			anim = animations['jumpR']
-		end
+    if fly == true then
+        if side == -1 then
+            anim = animations['jumpL']
+        else
+            anim = animations['jumpR']
+        end
     end
 
 
@@ -100,7 +100,7 @@ function Player.land()
 end
 
 function Player.fly()
-	fly = true
+    fly = true
 end
 
 function Player.addAnim(name, w, h, left, top, n, time)
@@ -112,32 +112,41 @@ end
 
 function Player.animationUpdate(dt)
     if run == 0 and fly == false then
-    	if side == -1 then 
-    		animations['standL']:update(dt) 
-    	else
-    		animations['standR']:update(dt)
-    	end
+        if side == -1 then 
+            animations['standL']:update(dt) 
+        else
+            animations['standR']:update(dt)
+        end
     elseif run == 1 and side == 1 and fly == false then
         animations['runR']:update(dt)
     elseif run == 1 and side == -1 and fly == false then
         animations['runL']:update(dt)
     else
-    	t = t + dt
+        t = t + dt
     end
 
 
-    if t < 0.09 and fly == true and jump == true then
-    	if side == -1 then
-    		animations['jumpL']:gotoFrame(1)
-    	else
-    		animations['jumpR']:gotoFrame(1)
-    	end
+
+    --------> whaaaat <--------
+    if fly == true and jump == false then
+        print('fly jump')
+        if side == -1 then
+            animations['jumpL']:gotoFrame(2)
+        else
+            animations['jumpR']:gotoFrame(2)
+        end
+    elseif t < 0.09 and fly == true and jump == true then
+        if side == -1 then
+            animations['jumpL']:gotoFrame(1)
+        else
+            animations['jumpR']:gotoFrame(1)
+        end
     elseif t > 0.09 or fly == true then
-    	if side == -1 then
-    		animations['jumpL']:gotoFrame(2)
-    	else
-    		animations['jumpR']:gotoFrame(2)
-    	end
+        if side == -1 then
+            animations['jumpL']:gotoFrame(2)
+        else
+            animations['jumpR']:gotoFrame(2)
+        end
     end
 end
 
@@ -149,7 +158,7 @@ end
 
 function Player.keypressed(key, scancode, isrepeat)
     if key == 'up' and (fly == false or dj == false) then
-    	jump = true
+        jump = true
         Player.speedY = -400-- -500
         dj = fly
         fly = true
