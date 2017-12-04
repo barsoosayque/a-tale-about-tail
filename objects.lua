175071d4b699ba990d5076917ff97dda7885aff7
Alexandr <leidenn2509@gmail.com>
2017-12-04 10:09:35 +0700

    obj

---
+++ objects.lua
@@ -0,0 +1,25 @@
+local Objects = {}
+
+
+
+
+
+function Objects.newObject(name, x, y, width, height)
+	local obj = {}
+	obj.name = name
+	obj.x, obj.y = x, y
+	obj.width, obj.height = width, height
+	obj.speedX, obj.speedY = 0, 0
+	obj.full = true
+
+	obj.filter = function(item, other)
+		if other.name == 'palyer' then
+			return 'cross'
+		end
+	end
+
+	return obj
+
+end
+
+return Objects
