be54acafdea686e18367d7d15e8d869c5480ae13
barsoosayque <shtoshich@gmail.com>
2017-12-05 02:26:39 +0700

    late fun

--- stage.lua
+++ stage.lua
@@ -4,6 +4,7 @@ local gamera = require('lib/gamera')
 local bump = require('lib/bump')
 local object = require('objects')
 local enemys = require('enemy')
+local music = require('music')
 
 local world = bump.newWorld(16)
 
@@ -202,12 +203,12 @@ function Stage.update(dt)
         for i = 1, len do
             local other = cols[i].other
             local item = cols[i].item
-            
+
             local name = other.name
             if item.name == 'player' and name == 'enemy' then
                 print('ded')
                 Stage.reset()
-            end 
+            end
 
             if name == 'treasure' and entitie.name == 'player' then
                 other.full = false
@@ -216,6 +217,7 @@ function Stage.update(dt)
                 local cost = 5
                 entitie.bag = entitie.bag + cost
                 entitie.speed = math.max(entitie.speed - cost, 50)
+                music.effect('pickup')
                 -- print('Coin:' .. tostring(entitie.bag))
             end
 
@@ -268,7 +270,7 @@ function Stage.reset()
     world:remove(entities['player'])
     entities['player'].reset(spawn.x, spawn.y)
     world:add(entities['player'], spawn.x, spawn.y, entities['player'].width, entities['player'].height)
-    
+
     for _, entitie in pairs(entities) do
         if entitie.name ~= 'player' then
             world:remove(entitie)
@@ -278,7 +280,7 @@ function Stage.reset()
     end
     for _, obj in pairs(objects) do
         if obj.full == false then
-            world:add(obj, obj.x, obj.y, obj.width, obj.height) 
+            world:add(obj, obj.x, obj.y, obj.width, obj.height)
             obj:reset()
         end
 
